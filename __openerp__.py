39d1113723bb8a88d2ca5b6c641e2f5203ca0cfe
Jhonny Martinez E <jhon18.26@gmail.com>
2015-02-18 17:18:51 -0500

    modelos, vistas, relaciones entre tablas

---
+++ __openerp__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "openacademy",
+
+    'summary': """
+        Modulo de prueba""",
+
+    'description': """
+        Modulo de prueba
+    """,
+
+    'author': "Jhonny Martínez Espinoza",
+    'website': "http://www.yourcompany.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Test',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'templates.xml',
+        'views/openacademy.xml'
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo.xml',
+    ],
+}
