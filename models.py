b478a9a14d8dd8e4ed160153141cf6b066e04ad0
Jhonny Martinez E <jhon18.26@gmail.com>
2015-02-19 00:38:14 -0500

    Campo compmutado y progressbar a las sesiones de los cursos

--- models.py
+++ models.py
@@ -23,6 +23,17 @@ class session(models.Model):
 		domain=['|', ('instructor', '=', True), ('category_id.name', 'ilike', 'Teacher')])
 	course_id = fields.Many2one('openacademy.course', ondelete='cascade', string="Course", required=True)
 	attendee_ids = fields.Many2many('res.partner', string="Attendees")
+
+	taken_seats = fields.Float(string="Taken Seats", compute='_taken_seats')
+
+	@api.one #reemplaza el uso del bucle en el metodo
+	@api.depends('seats', 'attendee_ids')
+	def _taken_seats(self):
+		if not self.seats:
+			self.taken_seats = 0.0
+		else:
+			self.taken_seats = 100.0 * len(self.attendee_ids) / self.seats
+
 # class openacademy(models.Model):
 #     _name = 'openacademy.openacademy'
 
